5a5e5357f88706aa946169f45bfb3831da69774b
MohamadNach <mohamad.nachawati@student.lab.fi>
2022-04-25 09:48:06 +0300

    L9 Assignment

--- src/screens/mynotes.js
+++ src/screens/mynotes.js
@@ -1,35 +1,36 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { listNotes } from '../utils/myfs';
+import {View, Text, Button} from 'react-native';
+import {listNotes} from '../utils/myfs';
 
-export const MyNotes = ({ navigation }) => {
+export const MyNotes = ({navigation}) => {
+  const [notes, setNotes] = React.useState([]);
 
-    const [ notes, setNotes ] = React.useState([]);
+  React.useEffect(() => {
+    let isSubscribed = true;
+    listNotes().then(_notes => {
+      console.log({_notes});
+      if (isSubscribed) {
+        setNotes(_notes);
+      }
+    });
+    return () => (isSubscribed = false);
+  }, []);
 
-    React.useEffect(() => {
-        let isSubscribed = true;
-        listNotes().then((_notes) => {
-            console.log({_notes});
-            if (isSubscribed) {
-                setNotes(_notes)
-            }
-        });
-        return () => isSubscribed = false;
-    }, []);
+  const openEditor = note => {
+    console.log(`open note: ${note}`);
+    navigation.navigate('note-editor', {filename: note});
+  };
 
-    const openEditor = (note) => {
-        console.log(`open note: ${note}`);
-        navigation.navigate('note-editor', { filename: note });
-    }
-
-    return (<View>
-        <Text>my-notes</Text>
-        {
-            notes.map((note, i) => (<View key={`note-${i}`}>
-                <Button title={note} onPress={() => openEditor(note)}></Button>
-            </View>))
-        }
-    </View>);
-}
+  return (
+    <View>
+      <Text>my-notes</Text>
+      {notes.map((note, i) => (
+        <View key={`note-${i}`}>
+          <Button title={note} onPress={() => openEditor(note)}></Button>
+        </View>
+      ))}
+    </View>
+  );
+};
 
 export default MyNotes;
